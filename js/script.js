4a11a547bdf1c501cf1ad91a1cdf7a4858117cb4
Davide <d.sadotti@gmail.com>
2021-04-19 19:50:37 +0200

    Footer Completato

--- js/script.js
+++ js/script.js
@@ -9,10 +9,35 @@ var app = new Vue(
         'Blog',
         'Contact',
         'Portfolio'
-      ]
+      ],
+      social:[
+        'instagram',
+        'linkedin-in',
+        'facebook-f',
+        'twitter'
+      ],
+      link:[
+        'Services',
+        'Contact',
+        'Blog'
+      ],
+      resources:[
+        'Art Design',
+        'Computer',
+        'IT News',
+        'Networking',
+        'Web Security'
+      ],
+      contact:{
+          address: 'Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+          phone: '+99 (0) 101 0000 888',
+          email: 'info@yourdomain.com'
+        }
     },
     methods: {
-
+      toCapitalize: function(name){
+        return name.charAt(0).toUpperCase() + name.slice(1)
+      }
     }
   }
 );
