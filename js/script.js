8112a69e5a07029de12e88ff14a38e20451f0aeb
Davide <d.sadotti@gmail.com>
2021-04-21 18:09:17 +0200

    Section Blog e Testimonial Completate

--- js/script.js
+++ js/script.js
@@ -69,20 +69,53 @@ var app = new Vue(
           title: 'Amazon gets CCI nod to acquire 49% stake in',
           text: 'When, while the lovely valley teems with vapour around meand the meridian sun strike the upper surface.',
           date: 'April 21, 2019',
-          by: 'Paul'
+          by: 'Paul',
+          isActive: true
         },
         {
           title: 'Clearing Corporations get SEBI nod to invest in overnight funds',
           text: 'When, while the lovely valley teems with vapour around meand the meridian sun strike the upper surface.',
           date: 'April 21, 2019',
-          by: 'Paul'
+          by: 'Paul',
+          isActive: false
         },
         {
           title: 'Shabnam Dhillion, wife of RSSB chief Gurinder Dhillon, passes away in London',
           text: 'When, while the lovely valley teems with vapour around meand the meridian sun strike the upper surface.',
           date: 'April 21, 2019',
-          by: 'Paul'
+          by: 'Paul',
+          isActive: false
         }
+      ],
+      testimonial:[
+        {
+          image: 'img/szabo-viktor.png',
+          rate: 4,
+          text: 'When, while lovely valley teems with vapour around meand meridian sun strike the upper impenetrable foliage of my trees, and but a thousand.',
+          who: 'Szabo Viktor',
+          position: 'Marketing Management'
+        },
+        {
+          image: 'img/philipe-cavalcante.png',
+          rate: 3,
+          text: 'When, while lovely valley teems with vapour around meand meridian sun strike the upper impenetrable foliage of my trees, and but a thousand.',
+          who: 'Philipe Cavalcante',
+          position: 'Marketing Management'
+        },
+        {
+          image: 'img/fabio-spinelli.png',
+          rate: 5,
+          text: 'When, while lovely valley teems with vapour around meand meridian sun strike the upper impenetrable foliage of my trees, and but a thousand.',
+          who: 'Fabio Spinelli',
+          position: 'Marketing Management'
+        }
+      ],
+      client:[
+        'img/client-1.png',
+        'img/client-2.png',
+        'img/client-3.png',
+        'img/client-4.png',
+        'img/client-5.png',
       ],
       social:[
         'instagram',
@@ -111,6 +144,15 @@ var app = new Vue(
     methods: {
       toCapitalize: function(name){
         return name.charAt(0).toUpperCase() + name.slice(1)
+      },
+      hoverOpenBox: function(index){
+        for(var i = 0; i < this.blog.length; i++){
+          if(i == index){
+            this.blog[index].isActive = true;
+          }else{
+            this.blog[i].isActive = false
+          }
+        }
       }
     }
   }
